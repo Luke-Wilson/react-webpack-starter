65a12d42e024e6d3b579e2b23cf8e3daf66b8eae
Luke Wilson <wilson.palooza@gmail.com>
2017-02-02 21:23:02 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a basic React repo that uses Webpack and Babel.
+
+#Setup
+
+npm install
+
+#Webpack build and start server
+
+npm start
+
+#View app
+
+http://localhost:3003
---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('hello from app.js');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import StatefulComponent from './StatefulComponent';
+
+ReactDOM.render(StatefulComponent, document.getElementById('app'));
